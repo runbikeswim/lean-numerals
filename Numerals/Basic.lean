178961f98b4a3c0c8e18b95a8e24ad2eece642d0
stefan <stefan.kusterer@gmx.net>
2026-01-04 00:21:55 +0100

    got stuck in toNat_add_left_distrib

--- Numerals/Basic.lean
+++ Numerals/Basic.lean
@@ -161,7 +161,7 @@ theorem toNat_helper_acc_factor (digits : List Nat) (base factor acc : Nat)  :
     rw [ih (factor * base) (head * factor + acc)]
 
 @[simp]
-theorem toNat_helper_cons (digits : List Nat) (n base : Nat) :
+theorem toNat_helper_cons {digits : List Nat} {n base : Nat} :
   (toNat.helper (n::digits) base 1 0).snd = n + base * (toNat.helper digits base 1 0).snd := by
   rw [toNat.helper.eq_def]
   simp only [Nat.one_mul, Nat.mul_one, Nat.add_zero]
@@ -337,6 +337,10 @@ theorem addAux_noTrailingZeros_of_noTrailingZeros {a b : List Nat} (n : Nat) {ba
     let  := ih h2 h4
     exact noTrailingZeros_cons_of_ne_zero ((x + y + n) % base) h5 (ih h2 h4)
 
+theorem addAux_cons_eq_cons_addAux {x y : Nat} {xs ys : List Nat} (n : Nat) {base : Nat} (hb : 1 < base) :
+  addAux (x::xs) (y::ys) n base hb = ((x + y + n) % base)::(addAux xs ys ((x + y + n) / base) base hb) := by
+  rw [Numeral.addAux.eq_def]
+
 theorem addAux_eq_cons_zero_addAux_of_eq_nil_of_eq_nil {a b : List Nat} (n : Nat) {base : Nat}
   (han : a = []) (hbn : b = []) (hb : 1 < base) (hn : base ≤ n ∧ n % base = 0) :
   addAux a b n base hb = 0::(addAux a b (n / base) base hb) := by
@@ -378,6 +382,27 @@ theorem addAux_add_eq_append_addAux_addAux {a b : List Nat} (n m : Nat) {base : 
     rw [Nat.succ_eq_add_one] at gn
     rw [Nat.mod_eq_of_lt hn.right, ← gn, hde]
 
+theorem addAux_eq_of_nil_of_zero {a b : List Nat} (n : Nat) {base : Nat}
+  (halt : a.all (· < base)) (hbn : b = []) (hn : n = 0) (hb : 1 < base)  :
+  addAux a b n base hb = a := by
+  induction a with
+  | nil => rw [hbn, hn]; exact (addAux_eq_nil_iff hb).mpr (And.intro rfl (And.intro rfl rfl))
+  | cons head tail ih =>
+    rw [Numeral.addAux.eq_def]
+    match ga : head :: tail, gb : b, gn : n with
+    | [], [], 0 | [], [], k + 1 => contradiction
+    | x::xs, [], n' =>
+      have h1 : head = x ∧ tail = xs := List.cons_eq_cons.mp ga
+      have h2 : head < base ∧ tail.all (· < base) := allDigitsLtBase_cons.mp halt
+      have h3 : x < base ∧ xs.all (· < base) := by rwa [h1.left, h1.right] at h2
+      have h4 : x / base = 0 := Nat.div_eq_zero_iff.mpr (.inr h3.left)
+      have h5 : x % base = x := (Nat.mod_eq_iff_lt (Nat.ne_zero_of_lt hb)).mpr h3.left
+      simp only []
+      rw [hn, Nat.add_zero, h4, h5, ← h1.left, ← h1.right, ← hn, ih h2.right]
+    | [], y::ys, n | x::xs, y::ys, n => contradiction
+
+
+
 end AddAux
 
 section OfNat
@@ -402,23 +427,20 @@ theorem toNat_leftInverse_ofNat {n base : Nat} (hb : 1 < base) : toNat (ofNat n 
     else
       have h0 : 0 < n := Nat.pos_iff_ne_zero.mpr h
       have h1 : n / base * base ≤ n := Nat.div_mul_le_self n base
-      have h2 : n = n % base + (n / base) * base := by
-        rw [Nat.mod_eq_sub_div_mul, Nat.sub_add_cancel h1]
+      have h2 : n = n % base + (n / base) * base := by rw [Nat.mod_eq_sub_div_mul, Nat.sub_add_cancel h1]
       unfold ofNat toNat at ⊢ ih
       simp only at ⊢ ih
       if g: n % base = 0 then
         have h3 : base ≤ n := Nat.le_of_mod_lt (by rwa [← g] at h0)
         rw [addAux_eq_cons_zero_addAux_of_eq_nil_of_eq_nil n rfl rfl hb (And.intro h3 g)]
-        rw [toNat_helper_cons (addAux [] [] (n / base) base hb) 0 base]
-        rw [ih (n / base) (Nat.div_lt_self h0 hb)]
+        rw [toNat_helper_cons, ih (n / base) (Nat.div_lt_self h0 hb)]
         rw [Nat.zero_add, Nat.mul_div_eq_iff_dvd.mpr (Nat.dvd_of_mod_eq_zero g)]
       else
-        have h4 : 0 < n % base := Nat.pos_iff_ne_zero.mpr g
-        have h5 : n % base < base := Nat.mod_lt n (Nat.lt_trans (by decide) hb)
-        rw [h2, addAux_add_eq_append_addAux_addAux (n % base) (n / base) rfl rfl hb (And.intro h4 h5)]
-        rw [addAux_eq_singleton (n % base) rfl rfl hb (And.intro h4 h5), List.singleton_append]
-        rw [toNat_helper_cons (addAux [] [] (n / base) base hb) (n % base) base]
-        rw [ih (n / base) (Nat.div_lt_self h0 hb)]
+        have h3 : 0 < n % base := Nat.pos_iff_ne_zero.mpr g
+        have h4 : n % base < base := Nat.mod_lt n (Nat.lt_trans (by decide) hb)
+        rw [h2, addAux_add_eq_append_addAux_addAux (n % base) (n / base) rfl rfl hb (And.intro h3 h4)]
+        rw [addAux_eq_singleton (n % base) rfl rfl hb (And.intro h3 h4), List.singleton_append]
+        rw [toNat_helper_cons, ih (n / base) (Nat.div_lt_self h0 hb)]
         simp only [Nat.mul_comm]
 
 end OfNat
@@ -444,18 +466,18 @@ def add (a b : Numeral) (_: a.base = b.base) : Numeral where
   allDigitsLtBase := all_addAux_digits_lt_base 0 a.baseGtOne
   noTrailingZeros := addAux_noTrailingZeros_of_noTrailingZeros 0 a.noTrailingZeros b.noTrailingZeros a.baseGtOne
 
-theorem add_nil_iff_and_nil_nil (a b : Numeral) (h : a.base = b.base) :
+theorem add_nil_iff_and_nil_nil {a b : Numeral} (h : a.base = b.base) :
   (add a b h).digits = [] ↔ a.digits = [] ∧ b.digits = [] := by
   unfold add
   simp only [addAux_eq_nil_iff, true_and]
 
-theorem add_zero_iff_or_zero_zero (a b : Numeral) (h : a.base = b.base) :
+theorem add_zero_iff_or_zero_zero {a b : Numeral} (h : a.base = b.base) :
   (add a b h).digits = [0]
     ↔ a.digits = [0] ∧ b.digits = [] ∨ a.digits = [] ∧ b.digits = [0] ∨ a.digits = [0] ∧ b.digits = [0] := by
   unfold add
   simp only [addAux_eq_zero_iff, true_and]
 
-theorem add_comm (a b : Numeral) (h : a.base = b.base) : add a b h = add b a (by simp only [h]) := by
+theorem add_comm {a b : Numeral} (h : a.base = b.base) : add a b h = add b a (by simp only [h]) := by
   have hblt : (b.digits.all fun x ↦ decide (x < b.base)) = true := b.allDigitsLtBase
   have hblt' : (b.digits.all fun x ↦ decide (x < a.base)) = true := by rwa [← h] at hblt
   have hd : addAux a.digits b.digits 0 a.base a.baseGtOne = addAux b.digits a.digits 0 a.base a.baseGtOne :=
@@ -477,14 +499,14 @@ theorem hAdd_comm (a b : Numeral) : hAdd a b = hAdd b a := by
   unfold hAdd
   if  h : a.base = b.base then
     simp only [h]
-    exact add_comm a b h
+    exact add_comm h
   else
     if g : a.base < b.base then
       have g' : ¬ b.base < a.base := Nat.not_lt_of_gt g
       simp only [h, ↓reduceDIte, g, ↓reduceIte, eq_comm, g']
       have harb : (rebase a b.base b.baseGtOne).base = b.base :=
         rebase_base_eq_base a b.base b.baseGtOne
-      exact add_comm (rebase a b.base b.baseGtOne) b harb
+      exact add_comm harb
     else
       have he : a.base = b.base ↔ b.base = a.base := eq_comm
       have h' : ¬b.base = a.base := (iff_iff_iff_not_not.mp he).mp h
@@ -493,22 +515,48 @@ theorem hAdd_comm (a b : Numeral) : hAdd a b = hAdd b a := by
       simp only [h, g, g'', ↓reduceDIte, ↓reduceIte, eq_comm]
       have hbrb : (rebase b a.base a.baseGtOne).base = a.base :=
         rebase_base_eq_base b a.base a.baseGtOne
-      exact add_comm (rebase b a.base a.baseGtOne) a hbrb
+      exact add_comm hbrb
 
 instance instCommutativeHAddNumerals : Std.Commutative hAdd := ⟨hAdd_comm⟩
 instance instHAddNumerals : HAdd Numeral Numeral Numeral := ⟨hAdd⟩
 
-theorem toNat_add_left_distrib (a b : Numeral) (h : a.base = b.base) :
+theorem toNat_add_left_distrib_of_nil {a b : Numeral} (h : a.base = b.base) (hbn : b.digits = []) :
   toNat (add a b h) = a.toNat + b.toNat := by
-  have h0 : 0 < a.base := Nat.lt_trans (by decide) a.baseGtOne
-  induction ga : a.digits with
-  | nil => sorry
-  | cons head tail ih =>
-    simp only [add, toNat]
-    simp only [ga]
-    rw [Numeral.addAux.eq_def]
-    simp only []
-    sorry
+  simp only [add, toNat]
+  rw [Numeral.addAux.eq_def, hbn]
+  match ga: a.digits with
+  | [] =>
+    have : toNat.helper [] a.base 1 0 = (1, 0) := by rw [toNat_helper_nil]
+    simp_all only []
+  | x::xs =>
+    have h1 : a.digits.all (· < a.base) := a.allDigitsLtBase
+    have h2 : (x :: xs).all (· < a.base) := by rwa [ga] at h1
+    have h3 : x < a.base ∧ xs.all (· < a.base) := allDigitsLtBase_cons.mp h2
+    have h4 : x / a.base = 0 := Nat.div_eq_zero_iff.mpr (.inr h3.left)
+    have h5 : x % a.base = x := (Nat.mod_eq_iff_lt (Nat.ne_zero_of_lt a.baseGtOne)).mpr h3.left
+    have h6 : addAux xs [] 0 a.base a.baseGtOne = xs :=
+      addAux_eq_of_nil_of_zero 0 h3.right rfl rfl a.baseGtOne
+    have h7 : (toNat.helper [] b.base 1 0).snd = 0 := by rw [toNat_helper_nil]
+    simp only [] -- condense match
+    rw [Nat.add_zero, h4, h5, h6, h7, Nat.add_zero]
+
+theorem toNat_add_left_distrib {a b : Numeral} (h : a.base = b.base) :
+  toNat (add a b h) = a.toNat + b.toNat := by
+  match gb : b.digits with
+  | [] => exact toNat_add_left_distrib_of_nil h gb
+  | y::ys =>
+    induction ga : a.digits with
+    | nil =>
+      have h' : b.base = a.base := eq_comm.mp h
+      have : toNat (add b a h') = b.toNat + a.toNat := toNat_add_left_distrib_of_nil h' ga
+      rw [Nat.add_comm b.toNat a.toNat, add_comm h'] at this
+      exact this
+    | cons x xs ih =>
+      simp only [add, toNat, ga, gb]
+      simp only [add, toNat] at ih
+      rw [addAux_cons_eq_cons_addAux 0 a.baseGtOne, Nat.add_zero]
+      rw [toNat_helper_cons, toNat_helper_cons, toNat_helper_cons]
+      sorry
 
 end Add
 
