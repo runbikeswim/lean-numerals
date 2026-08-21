f285128282546c070d24644e3350f963a728b4c7
stefan <stefan.kusterer@gmx.net>
2026-08-21 11:37:20 +0200

    zero_equiv_add_iff_zero_equiv_and_zero_equiv ✅

--- Numerals/Add.lean
+++ Numerals/Add.lean
@@ -145,6 +145,32 @@ def hAdd {base : NatGtOne} (n m : TZNumeral base) : TZNumeral base where
 instance instHAddTZNumerals {base : NatGtOne} :
   HAdd (TZNumeral base) (TZNumeral base) (TZNumeral base) := ⟨hAdd⟩
 
+theorem hAdd_helper_nil_nil_eq {base : NatGtOne} {n : Nat} (h : n ≠ 0) :
+  hAdd.helper base [] [] n = FinBase.ofNat n :: hAdd.helper base [] [] (n / base.val) := by
+  match n with
+  | 0 => contradiction
+  | k + 1 => simp only [hAdd.helper]
+
+theorem hAdd_helper_nil_comm {base : NatGtOne} {a : List base.Fin} {n : Nat} :
+  hAdd.helper base a [] n = hAdd.helper base [] a n := by
+  induction a generalizing n with
+  | nil => rfl
+  | cons x xs ih => simp only [hAdd.helper, ih]
+
+theorem hAdd_helper_comm {base : NatGtOne} {a b : List base.Fin} {n : Nat} :
+  hAdd.helper base a b n = hAdd.helper base b a n := by
+  induction a generalizing b n with
+  | nil => simp only [hAdd_helper_nil_comm]
+  | cons x xs ih =>
+    match b, n with
+    | [], n => simp only [hAdd_helper_nil_comm]
+    | y::ys, n => simp only [hAdd.helper, Nat.add_comm, ih]
+
+theorem add_comm {base : NatGtOne} {a b : TZNumeral base} :
+  a + b = b + a := by
+  simp only [HAdd.hAdd, hAdd, eq_iff_digits_eq]
+  exact hAdd_helper_comm
+
 theorem hAdd_helper_eq_nil_iff {base : NatGtOne} {a b : List base.Fin} {n : Nat} :
   hAdd.helper base a b n = [] ↔ a = [] ∧ b = [] ∧ n = 0 := by
   constructor
@@ -168,6 +194,51 @@ theorem add_eq_zero_iff {base : NatGtOne} {a b : TZNumeral base} :
       hAdd_helper_eq_nil_iff.mpr (And.intro h.left (And.intro h.right rfl))
     exact this
 
+theorem not_equiv_helper_nil_hAdd_helper_nil_nil_of_ne_zero {base : NatGtOne} {n : Nat} (h : n ≠ 0) :
+  ¬ equiv.helper base [] (hAdd.helper base [] [] n) := by
+  induction n using Nat.strongRecOn with
+  | _ l ih =>
+    if g: l < base.val then
+      simp only [hAdd_helper_nil_nil_eq h, equiv.helper, not_and]
+      intro h1
+      have h2 : l % base.val = l := (Nat.mod_eq_iff_lt base.val_ne_zero).mpr g
+      have h3 : l = 0 := by
+        simp only [OfNat.ofNat, FinBase.ofNat, h2, Fin.eq_mk_iff_val_eq, Nat.zero_mod] at h1
+        assumption
+      exact absurd h3 h
+    else
+      simp only [hAdd_helper_nil_nil_eq h, equiv.helper, not_and]
+      intro h1
+      have h2 : 0 < l := Nat.pos_of_ne_zero h
+      have h3 : l / base.val < l := Nat.div_lt_self h2 base.property
+      have h4 : l / base.val ≠ 0 := Nat.div_ne_zero_iff.mpr (And.intro base.val_ne_zero (Nat.le_of_not_lt g))
+      exact ih (l / base.val) h3 h4
+
+theorem equiv_helper_nil_and_eq_zero_of_equiv_helper_nil_hAdd_helper_nil {base : NatGtOne}
+  {a : List base.Fin} {n : Nat} (h : equiv.helper base [] (hAdd.helper base [] a n)) :
+  equiv.helper base [] a ∧ n = 0 := by
+  induction a generalizing n with
+  | nil =>
+    if g : n = 0 then
+      exact And.intro equiv_helper_nil_nil g
+    else
+      exact absurd h (not_equiv_helper_nil_hAdd_helper_nil_nil_of_ne_zero g)
+  | cons x xs ih =>
+    simp only [hAdd.helper] at h
+    simp only [equiv.helper] at ⊢ h
+    have h1 : (↑x + n) % base.val = 0 := by
+      rw [FinBase.ofNat] at h
+      exact Eq.symm (Fin.eq_mk_iff_val_eq.mp (Eq.symm h.left))
+    have h2 : equiv.helper base [] xs ∧ ((↑x + n) / base.val) = 0 := ih h.right
+    have h3 : ↑x + n = 0 := by
+      rw [← Nat.mod_add_div (↑x + n) base.val, h1, h2.right, Nat.zero_add, Nat.mul_zero]
+    have h4 : x = 0 := Fin.eq_mk_iff_val_eq.mpr (Nat.add_eq_zero_iff.mp h3).left
+    exact And.intro (And.intro h4 h2.left) (Nat.add_eq_zero_iff.mp h3).right
+
+theorem equiv_helper_nil_of_equiv_helper_nil_hAdd_helper_nil {base : NatGtOne} {a : List base.Fin}
+  (h : equiv.helper base [] (hAdd.helper base [] a 0)) : equiv.helper base [] a :=
+  (equiv_helper_nil_and_eq_zero_of_equiv_helper_nil_hAdd_helper_nil h).left
+
 theorem equiv_helper_nil_hAdd_helper_nil_of_equiv_helper_nil {base : NatGtOne} {a : List base.Fin}
   (h: equiv.helper base [] a) : equiv.helper base [] (hAdd.helper base [] a 0) := by
   induction a with
@@ -180,23 +251,85 @@ theorem equiv_helper_nil_hAdd_helper_nil_of_equiv_helper_nil {base : NatGtOne} {
     simp only [h.left, Nat.add_zero, FinBase.ofNat, OfNat.ofNat, Nat.zero_mod, Nat.zero_div, true_and]
     exact ih h.right
 
-theorem equiv_helper_nil_of_equiv_helper_nil_hAdd_helper_nil_of {base : NatGtOne} {a : List base.Fin}
-  (h : equiv.helper base [] (hAdd.helper base [] a 0)) : equiv.helper base [] a := by
-  induction a with
-  | nil => exact equiv_helper_nil_nil
-  | cons x xs ih =>
-    simp only [hAdd.helper, equiv.helper] at ⊢ h
-    simp only [FinBase.ofNat, Nat.add_zero, Nat.mod_eq_of_lt (Fin.isLt x), Fin.eta] at h
-    simp only [h.left, true_and, FinBase.ofNat, Nat.zero_mod, OfNat.ofNat, Nat.zero_div] at ⊢ h
-    exact ih h
+theorem equiv_helper_nil_hAdd_helper_nil_nil_of_eq_zero {base : NatGtOne} {n : Nat} (h : n = 0) :
+  equiv.helper base [] (hAdd.helper base [] [] n) := by
+  rw [h]
+  exact equiv_helper_nil_hAdd_helper_nil_of_equiv_helper_nil equiv_helper_nil_nil
 
-theorem equiv_helper_nil_hAdd_helper_nil_iff_equiv_helper_nil {base : NatGtOne} {a : List base.Fin} :
-  equiv.helper base [] (hAdd.helper base [] a 0) ↔ equiv.helper base [] a :=
-  Iff.intro equiv_helper_nil_of_equiv_helper_nil_hAdd_helper_nil_of equiv_helper_nil_hAdd_helper_nil_of_equiv_helper_nil
+theorem equiv_helper_nil_hAdd_helper_nil_iff_equiv_helper_nil {base : NatGtOne} {a : List base.Fin} {n : Nat}:
+  equiv.helper base [] (hAdd.helper base [] a n) ↔ equiv.helper base [] a ∧ n = 0 := by
+  constructor
+  · exact equiv_helper_nil_and_eq_zero_of_equiv_helper_nil_hAdd_helper_nil
+  · intro h
+    rw [h.right]
+    exact equiv_helper_nil_hAdd_helper_nil_of_equiv_helper_nil h.left
 
 theorem zero_equiv_zero_add_iff_zero_equiv {base : NatGtOne} {a : TZNumeral base} : 0 ≈ (0 + a) ↔ 0 ≈ a := by
   simp only [equiv, HAdd.hAdd, hAdd, OfNat.ofNat, ofNat, prune_nil_zero_eq_zero]
-  exact @equiv_helper_nil_hAdd_helper_nil_iff_equiv_helper_nil base a.digits
+  exact
+    Iff.intro
+      (fun t ↦ ((@equiv_helper_nil_hAdd_helper_nil_iff_equiv_helper_nil base a.digits 0).mp t).left)
+      (fun t ↦ ((@equiv_helper_nil_hAdd_helper_nil_iff_equiv_helper_nil base a.digits 0).mpr (And.intro t rfl)))
+
+theorem equiv_helper_nil_hAdd_helper_of_equiv_helper_nil_and_equiv_helper_nil {base : NatGtOne} {a b : List base.Fin}
+  (h: equiv.helper base [] a ∧ equiv.helper base [] b) : equiv.helper base [] (hAdd.helper base a b 0) := by
+  induction a generalizing b with
+  | nil => exact equiv_helper_nil_hAdd_helper_nil_of_equiv_helper_nil h.right
+  | cons x xs ih =>
+    match b with
+    | [] =>
+      rw [hAdd_helper_comm]
+      exact equiv_helper_nil_hAdd_helper_nil_of_equiv_helper_nil h.left
+    | y::ys =>
+      simp only [hAdd.helper, equiv.helper, Nat.add_zero] at ⊢ h
+      have h1 : ↑x + ↑y = (0 : Nat) := by
+        simp only [h.left.left, h.right.left, Nat.add_eq_zero_iff, Fin.val_eq_zero_iff, and_self]
+        rfl
+      simp only [h1, FinBase.ofNat, OfNat.ofNat, Nat.zero_div, FinBase.ofNat, Nat.zero_mod, true_and]
+      exact ih (And.intro h.left.right h.right.right)
+
+theorem equiv_helper_nil_and_equiv_helper_nil_and_eq_zero_of_equiv_helper_nil_hAdd_helper {base : NatGtOne} {a b : List base.Fin} {n : Nat}
+  (h : equiv.helper base [] (hAdd.helper base a b n)) : equiv.helper base [] a ∧ equiv.helper base [] b ∧ n = 0 := by
+  induction a generalizing b n with
+  | nil =>
+    have : equiv.helper base [] b ∧ n = 0 := equiv_helper_nil_and_eq_zero_of_equiv_helper_nil_hAdd_helper_nil h
+    exact And.intro equiv_helper_nil_nil this
+  | cons x xs ih =>
+    match b with
+    | [] =>
+      rw [hAdd_helper_comm] at h
+      have : equiv.helper base [] (x::xs) ∧ n = 0 := equiv_helper_nil_and_eq_zero_of_equiv_helper_nil_hAdd_helper_nil h
+      exact And.intro this.left (And.intro  equiv_helper_nil_nil this.right)
+    | y::ys =>
+      simp only [hAdd.helper, equiv.helper] at ⊢ h
+      have h1 : (↑x + ↑y + n) % base.val = 0 := by
+        rw [FinBase.ofNat] at h
+        exact Eq.symm (Fin.eq_mk_iff_val_eq.mp (Eq.symm h.left))
+      have h2 : equiv.helper base [] xs ∧ equiv.helper base [] ys ∧ ((↑x + ↑y+ n) / base.val) = 0 := ih h.right
+      have h3 : ↑x + ↑y + n = 0 := by
+        rw [← Nat.mod_add_div (↑x + ↑y + n) base.val, h1, h2.right.right, Nat.zero_add, Nat.mul_zero]
+      have h4 : n = 0 := (Nat.add_eq_zero_iff.mp h3).right
+      have h5 : x = 0 ∧ y = 0 := by
+        rw [h4, Nat.add_zero, Nat.add_eq_zero_iff] at h3
+        rwa [← @Fin.eq_mk_iff_val_eq base.val x 0 base.val_pos, ← @Fin.eq_mk_iff_val_eq base.val y 0 base.val_pos] at h3
+      have h6 : x = 0 ∧ equiv.helper base [] xs := And.intro h5.left h2.left
+      have h7 : y = 0 ∧ equiv.helper base [] ys := And.intro h5.right h2.right.left
+      exact And.intro h6 (And.intro h7 h4)
+
+theorem equiv_helper_nil_hAdd_helper_iff_equiv_helper_nil_and_equiv_helper_nil_and_eq_zero {base : NatGtOne} {a b : List base.Fin} {n : Nat}:
+  equiv.helper base [] (hAdd.helper base a b n) ↔ equiv.helper base [] a ∧ equiv.helper base [] b ∧ n = 0 := by
+  constructor
+  · exact equiv_helper_nil_and_equiv_helper_nil_and_eq_zero_of_equiv_helper_nil_hAdd_helper
+  · intro h
+    rw [h.right.right]
+    exact equiv_helper_nil_hAdd_helper_of_equiv_helper_nil_and_equiv_helper_nil (And.intro h.left h.right.left)
+
+theorem zero_equiv_add_iff_zero_equiv_and_zero_equiv {base : NatGtOne} {a b : TZNumeral base} : 0 ≈ (a + b) ↔ 0 ≈ a ∧ 0 ≈ b := by
+  simp only [equiv, HAdd.hAdd, hAdd, OfNat.ofNat, ofNat, prune_nil_zero_eq_zero]
+  exact
+    Iff.intro
+      (fun t ↦ let r := equiv_helper_nil_hAdd_helper_iff_equiv_helper_nil_and_equiv_helper_nil_and_eq_zero.mp t; And.intro r.left r.right.left)
+      (fun t ↦ equiv_helper_nil_hAdd_helper_iff_equiv_helper_nil_and_equiv_helper_nil_and_eq_zero.mpr (And.intro t.left (And.intro t.right rfl)))
 
 end Add
 
